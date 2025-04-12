0adf9ac85b661e540b5186cc4059a6afa46d7f0e
VIVOBOOK-HIM\HIM <fikri.rohim@sigma.co.id>
2025-04-12 07:04:33 +0700

    init repo

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+<h1>Welcome to your library project</h1>
+<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
---
+++ svelte.config.js
@@ -0,0 +1,19 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const config = {
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html', // may differ from host to host
+			precompress: false,
+			strict: true
+		})
+	}
+};
+
+export default config;
